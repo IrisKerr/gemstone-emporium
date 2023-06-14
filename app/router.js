8ded93dcfbd2b5c198e7848d7c06a8d7a054ecb5
IrisKerr <iris.kerr@hotmail.com>
2023-06-14 19:49:05 +0200

    / route setup

--- app/router.js
+++ app/router.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const shopController = require('./controllers/shopControllers');
 const router = express.Router();
+
+// controllers
+const shopController = require('./controllers/shopControllers');
+
+// affichage accueil
+router.get('/', shopController/index);
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+// configuration express
 const express = require("express");
 const app = express();
 
+// setup des views engine
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/app/views");
-
 app.use(express.static(`${__dirname}/public`));
 
 
