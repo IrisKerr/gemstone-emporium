37a6383536e0f12e221fa7674292e11ac65b4d1c
IrisKerr <iris.kerr@hotmail.com>
2023-06-15 19:42:40 +0200

    partials add

--- app/router.js
+++ app/router.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const shopController = require('./controllers/shopControllers');
 const router = express.Router();
 
 // controllers
 const shopController = require('./controllers/shopControllers');
 
 // affichage accueil
-router.get('/', shopController/index);
+router.get('/', shopController.index);
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // configuration express
 const express = require("express");
 const app = express();
+const router = require('./app/router');
 
 // setup des views engine
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/app/views");
 app.use(express.static(`${__dirname}/public`));
 
+app.use(router);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
